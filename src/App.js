8956a0af32086fa6763c9159221dd2eae4266352
Emely Garcia <garciam.emm@gmail.com>
2020-08-10 01:17:11 -0600

    interfaz en componente app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+//material
+import Button from '@material-ui/core/Button';
+import { Container, Grid, TextField } from '@material-ui/core';
+
+const CodeContainer = (props) => {
+  return (
+    <div className="container-xml" >
+      <code>
+        <TextField fullWidth className="textarea"
+          label={props.placeholder} multiline rows={20} variant="outlined"
+        />
+      </code>
+    </div>
+  );
+}
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="title">Derivador Simbólico XSLT</h1>
       </header>
+      <Container>
+        <Grid container direction="column" justify="center" alignItems="center">
+          <Grid container direction="row">
+            <Grid item xs={6}>
+              <CodeContainer xmlText="Holiiiii" />
+            </Grid>
+            <Grid item xs={6}>
+              <CodeContainer xmlText="Holiiiii" />
+            </Grid>
+          </Grid>
+          <Button className="btn-calc" variant="contained" color="primary">Calcular</Button>
+        </Grid>
+      </Container>
     </div>
   );
 }
